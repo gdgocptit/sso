7e667bb0bbf9f14b0ce6c8b24c2b739aef39364d
sophiathedev <thedevguy1337@gmail.com>
2024-10-21 17:12:10 +0700

    initial commit

---
+++ sso_admin/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SsoAdminConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'sso_admin'
---
+++ sso_admin/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.http import JsonResponse
+
+def home(request):
+    return JsonResponse({
+        "ok": "ok"
+    })
